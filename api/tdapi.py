f99dd3e932ecac81413ccd44c4d80a2826284cb7
Chenxin Ma <machx9@gmail.com>
2020-08-31 13:58:58 -0700

    api

--- api/tdapi.py
+++ api/tdapi.py
@@ -151,6 +151,115 @@ class TDAPI:
                     time.sleep(0.1)
                     continue
 
+        loggerPath.info('TDapi: Begin to merge today\'s option data')
+        self.mergeOptionDfForOneDay(today_date)
+        loggerPath.info('TDapi: Begin to merge option data for all dates')
+        self.mergeOptionAllDays()
+        loggerPath.info('TDapi: Begin to detect anomaly option trades.')
+        self.anomalyOptionTradeDetector(today_date)
+
+
+
+    def mergeOptionDfForOneDay(self, date):
+
+        longTermThres = (pd.Timestamp(date) + pd.DateOffset(months=1)).strftime("%Y-%m-%d")
+
+        optSymb = [f[:-4] for f in listdir(datapath / 'historical_option_daily/single/{}/'.format(date)) 
+                         if isfile(join(datapath / 'historical_option_daily/single/{}/'.format(date), f)) and f.endswith('.csv')]
+
+        cols = ['symb','date','callVol','longCVol','putVol','longPVol','pcVolR','ttlVol','callOpen','longCOpen','putOpen','longPOpen','pcOpenR','ttlOpen','vol/Open','vol/Share','open/Share']
+
+        oS = pd.DataFrame(columns=cols)
+        for symb in tqdm(optSymb):
+            o0 = pd.read_csv(datapath / 'historical_option_daily/single/{}/{}.csv'.format(date,symb) )
+            d4 = o0.groupby('putCall')[['totalVolume','openInterest']].sum().unstack().values
+            tts = dic_fund[symb]['fundamental']['sharesOutstanding']
+            ttlV = d4[0] + d4[1]
+            ttlO = d4[2] + d4[3]
+            pcVR = d4[1] / d4[0] if d4[0] != 0 else None
+            pcOR = d4[3] / d4[2] if d4[2] != 0 else None
+            voR = ttlV / ttlO if ttlO != 0 else 0
+            vsR = ttlV * 100 / tts if tts != 0 else 0
+            osR = ttlO * 100 / tts if tts != 0 else 0
+            o0Long = o0[o0['expirationDate'] >= longTermThres]
+            if len(o0Long) > 0:
+                d4Long = o0Long.groupby('putCall')[['totalVolume','openInterest']].sum().unstack().values
+                longCallVol = d4Long[0]
+                longPutVol = d4Long[1]
+                longCallOpen = d4Long[2]
+                longPutOpen = d4Long[3]
+            else:
+                longCallVol, longPutVol, longCallOpen, longPutOpen = 0, 0, 0, 0
+                
+            line = [symb, date, d4[0], longCallVol, d4[1], longPutVol, pcVR, ttlV, 
+                    d4[2], longCallOpen, d4[3], longPutOpen, pcOR, ttlO, 
+                    voR, vsR, osR]
+            oS.loc[len(oS)] = line
+
+        oS[['pcVolR','pcOpenR','vol/Open','vol/Share','open/Share']]\
+        = oS[['pcVolR','pcOpenR','vol/Open','vol/Share','open/Share']].astype(float)
+        oS = oS.round(3)
+        oS.to_csv(datapath / 'historical_option_daily/merge/{}.csv'.format(date), index=False, float_format='%.3f')
+
+
+
+    def mergeOptionAllDays(self):
+
+        dates = [f for f in listdir(datapath / 'historical_option_daily/single/') 
+                         if not isfile(join(datapath / 'historical_option_daily/single/', f))]
+        oDaily = {}
+        for date in dates:
+            oDaily[date] = pd.read_csv(datapath / 'historical_option_daily/merge/{}.csv'.format(date))
+        oAll = pd.concat([oDaily[date] for date in oDaily])
+
+        oAll.sort_values(['symb','date']).to_csv(datapath / 'historical_option_daily/all.csv', 
+                                            index=False, float_format='%.3f')
+
+
+
+    def anomalyOptionTradeDetector(self, todayDate):
+
+        colCheck = ['longCVol','longPVol','longCOpen','longPOpen']
+        watch = set()
+        longTermThres = (pd.Timestamp(date) + pd.DateOffset(months=1)).strftime("%Y-%m-%d")
+
+        cols = ['date','symb', 'detectedColumn', 'todayValue','pastMean','stockPriceChg','stockVolChg','optionName']
+        oWatch = pd.DataFrame(columns=cols)
+        for symb, oSymb in oAll.groupby('symb'):
+            oSymb = oSymb.sort_values('date')
+            todayRow = oSymb.iloc[-1]
+            prevRows = oSymb.iloc[-20:-1]
+            for col in colCheck:
+                if col.endswith('Vol') and todayRow[col] > prevRows[col].mean() * 3 and todayRow[col] > 1000 \
+                or col.endswith('Open') and todayRow[col] > prevRows[col].mean() * 1.5 and todayRow[col] > 10000:
+                    
+                    oSingle = pd.read_csv(datapath / 'historical_daily/single/{}.csv'.format(symb))
+                    dayChange = (oSingle.iloc[-1]['close'] / oSingle.iloc[-2]['close'] - 1) * 100
+                    volIncrease = oSingle.iloc[-1]['volume'] / oSingle.iloc[-21:-1]['volume'].mean()
+                    oOption = pd.read_csv(datapath / 'historical_option_daily/single/{}/{}.csv'.format(todayDate, symb))
+                
+                    longTermThres = (pd.Timestamp(todayDate) + pd.DateOffset(months=1)).strftime("%Y-%m-%d")
+                    o0Long = oOption[oOption['expirationDate'] >= longTermThres]
+                    o0LongC = o0Long[o0Long['putCall'] == 'CALL']
+                    o0LongP = o0Long[o0Long['putCall'] == 'PUT']
+                    if col[4] == 'C':
+                        bigOrder = o0LongC[o0LongC['totalVolume'] > todayRow[col] * 0.5]['description']
+                    elif col[4] == 'P':
+                        bigOrder = o0LongP[o0LongP['totalVolume'] > todayRow[col] * 0.5]['description']
+                        
+                    if len(bigOrder) > 0:
+                        theOne = bigOrder.values[0]
+                    else:
+                        theOne = ''
+        #             if abs(dayChange) < 3:
+                    row = [todayDate, symb, col, todayRow[col], '{0:.2f}'.format(prevRows[col].mean()), 
+                           '{0:.2f}'.format(dayChange), '{0:.2f}'.format(volIncrease), theOne]
+                    print(row[1:])
+                    oWatch.loc[len(oWatch)] = row
+
+        oWatch.to_csv(datapath / 'historical_option_daily/watch/{}.csv'.format(todayDate),index=False )
+                        
+
 
     def pullFundamentalForAll(self):
 
